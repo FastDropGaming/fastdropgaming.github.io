3e5750d13ec76185b4b402a936062f6d13303917
FastDropGaming <fastdropgaming@gmail.com>
2022-07-25 16:14:01 +0200

    Tja

---
+++ animation.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+    $('.html,.css,.js').css('margin','8px 3px 0 0');
+    $('.bar_anim1').css('width',0);
+    $('.bar_anim2').css('width',0);
+    $('.bar_anim3').css('width',0)
+    $('.file').css({'top':'calc(50% + 15px)','opacity':1});
+  },500)
